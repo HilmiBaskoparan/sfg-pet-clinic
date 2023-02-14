b4b90499e3f971985efb9038d1723f6baf0da2b1
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-02-14 10:43:46 +0300

    Adding POJOs. Closes #2

---
+++ src/main/java/hilmi/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package hilmi.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/hilmi/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package hilmi.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
