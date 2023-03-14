c60834ffac53cc4805bc28665281a2920d80d677
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-03-14 15:09:05 +0300

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/hilmi/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/hilmi/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package hilmi.springframework.sfgpetclinic.controllers;
 
+import hilmi.springframework.sfgpetclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerServiceMap ownerServiceMap;
+
+    public OwnerController(OwnerServiceMap ownerServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerServiceMap.findAll());
+
         return "owners/index";
     }
 
