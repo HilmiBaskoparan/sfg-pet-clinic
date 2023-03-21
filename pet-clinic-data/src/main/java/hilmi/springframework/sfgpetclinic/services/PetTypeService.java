6cf12dc344b8ee9968e812aa19fb07ac51f3b4c0
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-03-21 15:37:01 +0300

    Added PetTypeService and PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package hilmi.springframework.sfgpetclinic.services;
+
+import hilmi.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
