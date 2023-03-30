653c6a885304c1d7aa845e8a21e6c644a311cccb
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-03-30 15:41:40 +0300

    Adding Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/model/PetType.java
@@ -3,7 +3,13 @@ package hilmi.springframework.sfgpetclinic.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
