4b45dc88209d37d2351ef95906e5733617abaf0a
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-04-12 02:56:44 +0300

    Impl find owner. Closes #54

--- pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,11 @@ package hilmi.springframework.sfgpetclinic.repositories;
 import hilmi.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/hilmi/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,11 @@ package hilmi.springframework.sfgpetclinic.services;
 
 import hilmi.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
